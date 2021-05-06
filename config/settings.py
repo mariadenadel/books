9c84434c12513df2e8f4e018d552ca8deb0d37d9
MariiaDenysova <maria.denadel@gmail.com>
2021-05-06 15:18:59 +0200

    ch6

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party
+    'crispy_forms',
+
     # Local
     'accounts',
     'pages',
@@ -130,6 +133,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')), )
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 class HomepageTests(SimpleTestCase):
 
@@ -25,4 +25,29 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(
             view.func.__name__,
             HomePageView.as_view().__name__
+        )
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('about')
+        self.responce = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.responce.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.responce, 'about.html')
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.responce, 'About Page')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.responce, 'Hi! I should not be here!')
+
+    def test_aboutpage_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
         )
